19afbac48f379dbf87ff81af0b976af3c4e59fca
oyible <134149337+oyible@users.noreply.github.com>
2023-05-29 17:25:12 +0000

    Add files via upload

---
+++ eGoFintech.js
@@ -0,0 +1,10 @@
+// let t1 = gsap.timeline();
+
+// ti.timeline(default{duration: 1.2s, ease: 1s, reapet: 4s})
+
+
+let humber = document.querySelector('menu_click', onclick()=>{
+    document.addEventListener(
+        
+    )
+})
